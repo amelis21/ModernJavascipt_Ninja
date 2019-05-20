14dd22a8ee7d186a0c69541e56ff9ba71c8d93dd
amelis21 <anostha@gmail.com>
2019-05-20 22:12:16 +0300

    finish section 4

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -1,6 +1,6 @@
-function greet(){
-    console.log('Hello there');
-}
+// function greet(){
+//     console.log('Hello there');
+// }
 
 // greet();
 // greet();
@@ -20,5 +20,31 @@ function greet(){
 // console.log(area);
 
 
-const calcArea = radius =>  3.14 * radius**2;
-console.log(calcArea(2));
+// const calcArea = radius =>  3.14 * radius**2;
+// console.log(calcArea(2));
+
+// const myFunc = callbackFunc => {
+//     let value = 30;
+//     callbackFunc(value);
+// }
+
+// myFunc(value => {
+//     console.log(value);
+// });
+
+const people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+const ul = document.querySelector(".people");
+
+const logPerson = (person, index) =>{
+    console.log(index, person);
+}
+let html = ``;
+
+// people.forEach(logPerson);
+people.forEach(function(person){
+    html+=`<li style="color: purple">${person}</li>`;
+});
+
+
+console.log(html);
+ul.innerHTML = html;
