ca03ded10fad1b359d97f7d3eaa45c622b4a9645
amelis21 <anostha@gmail.com>
2019-05-22 19:22:45 +0300

    finish section 6

---
+++ chapter_6/sandbox.js
@@ -0,0 +1,21 @@
+// const para = document.querySelector('p');
+
+// console.log(para);
+
+// get an element by ID
+// const title = document.getElementById("page-title");
+// console.log(title);
+
+// // get ele,emts by classname
+// const errors = document.getElementsByClassName("error");
+// console.log(errors);
+
+// // get elements by their tag name
+// const paras = document.getElementsByTagName('p');
+// console.log(paras);
+// console.log(paras[1]);
+
+const link = document.querySelector('a');
+console.log(link.getAttribute("href"));
+console.log(link.style);
+link.style.fontSize = '60px';
