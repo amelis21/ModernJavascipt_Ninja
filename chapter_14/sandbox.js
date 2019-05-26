1d5ca7f550690e3ac651dfd252e30f9aaac687f4
amelis21 <anostha@gmail.com>
2019-05-26 09:51:36 +0300

    localstorage

---
+++ chapter_14/sandbox.js
@@ -0,0 +1,8 @@
+// store data in local storage
+// localStorage.setItem('name', 'mario');
+// localStorage.setItem('age', 50);
+
+// get data from local storage
+let name = localStorage.getItem('name');
+let age = localStorage.getItem('age');
+console.log(name, age);
